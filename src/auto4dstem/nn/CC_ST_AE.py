55ff93b075ee434e9bbca0972e10ead2cf10b2b8
yig319 <yig319@lehigh.edu>
2024-02-27 06:32:51 +0000

    documents of CC-ST-AE

--- src/auto4dstem/nn/CC_ST_AE.py
+++ src/auto4dstem/nn/CC_ST_AE.py
@@ -44,6 +44,7 @@ def revise_size_on_affine_gpu(
     coef=1.5,
     pare_reverse=False,
     affine_mode="bicubic",
+    dot_size = 4,
 ):
     """function for revise size of diffraction spots
 
@@ -58,6 +59,7 @@ def revise_size_on_affine_gpu(
         coef (int, optional): the parameter to control the value of threshold for COM operation. Defaults to 1.5.
         pare_reverse (bool, optional): switch multiplying or dividing adj_para . Defaults to False.
         affine_mode(string, optional): set the affine mode to function F.affine_grid(). Defaults 'bicubic'.
+        dot_size(int, optional): set the radius of the circle region for adjust intensity value if adjust parameter is not None. Defaults 4.
 
     Returns:
         torch.tenors: image after revise operation
@@ -67,10 +69,11 @@ def revise_size_on_affine_gpu(
     np_img = np.zeros([radius * 2, radius * 2])
     
     # crop small circle only include diffraction spots
-    dot_size = int(4 * image.shape[-1] / 200) # TODO make these parameters adjustable
+    dot_size = int(dot_size)
     small_square_mask = mask_function(
         np_img, radius=dot_size, center_coordinates=(radius, radius)
     )
+    
     small_square_mask = torch.tensor(small_square_mask, dtype=torch.bool).to(device)
 
     img = torch.clone(image).to(device)
@@ -171,12 +174,9 @@ def revise_size_on_affine_gpu(
 
 
 class conv_block(nn.Module):
-    """_summary_ 
-
-    Args:
-        nn (_type_): _description_
-    """    
-    # TODO: add docstring
+    """
+        nn.Module class of Residual Neural Network
+    """   
     
     def __init__(self, 
                 t_size, 
@@ -211,7 +211,7 @@ class conv_block(nn.Module):
         Returns:
             Tensor: output tensor
         """
-        # TODO add description
+        # Use Residual structure to concatenate input tensor to output of 3 convolutional layers
         x_input = x
         out = self.cov1d_1(x)
         out = self.relu_1(out)
@@ -227,11 +227,8 @@ class conv_block(nn.Module):
 
 class identity_block(nn.Module):
     """
-
-    Args:
-        nn (_type_): _description_
+        nn.Module class of Identity Neural Network
     """    
-    #TODO
     
     def __init__(self, t_size, n_step):
         """Initializes the identity block
@@ -264,13 +261,10 @@ class identity_block(nn.Module):
 
 
 class Affine_Transform(nn.Module):
-    """_summary_
-
-    Args:
-        nn (_type_): _description_
+    """
+        nn.Module class to return 3 type of affine transformation matrices (scale and shear, rotation, translation) and
+        a adjust parameter to change pixel intensity in mask region.
     """    
-    
-    # TODO add docstring
     
     def __init__(
         self,
@@ -289,8 +283,7 @@ class Affine_Transform(nn.Module):
         adj_mask_para=0,
         verbose=False,
     ):
-        # TODO add summary
-        """_summary_ 
+        """ 
 
         Args:
             device (torch.device): set the device to run the model
@@ -337,27 +330,33 @@ class Affine_Transform(nn.Module):
         Returns:
             Tensor: affine matrix and adjust parameters
         """
-        #TODO add more detailed line by line comments
         
-        # determine the type of affine transform and create matrix according to it
+        # if there's scale transformation, scale x and scale y should be corresponding index of the tensor out
         if self.scale:
             scale_1 = self.scale_limit * nn.Tanh()(out[:, self.count]) + 1
             scale_2 = self.scale_limit * nn.Tanh()(out[:, self.count + 1]) + 1
+            
+            # update count value to switch index for affine parameter calculation  
             self.count += 2
+            
+        # if there's no scale transformation, scale x and scale y should be 1
         else:
             scale_1 = torch.ones([out.shape[0]]).to(self.device)
             scale_2 = torch.ones([out.shape[0]]).to(self.device)
 
-
+        # if there's rotation transformation, rotation value should be corresponding index of the tensor out
         if self.rotation:
+            
+            # if exists pretrained rotate value, keep optimizing on it.  
             if rotate_value != None:
-                # use large mask no need to limit to too small range
-
+                
+                # if using large mask no need to set rotation_limit to too small range
                 rotate = rotate_value.reshape(
                     out[:, self.count].shape
                 ) + self.rotation_limit * nn.Tanh()(out[:, self.count])
 
             else:
+                # if rotate_clockwise is True, ReLU is added to rotate value; otherwise add rotation limitation to it.
                 if self.rotate_clockwise:
                     rotate = nn.ReLU()(out[:, self.count])
                 else:
@@ -365,14 +364,18 @@ class Affine_Transform(nn.Module):
 
             self.count += 1
 
+        # if there's no rotation transformation, rotate value should be 0
         else:
             rotate = torch.zeros([out.shape[0]]).to(self.device)
 
             if self.verbose:
                 print(self.count)
-
+        
+        # if there's shear transformation, shear parameter should be corresponding index of the tensor out
         if self.shear:
             
+            # if Symmetric is true, shear xy = shear yx
+            # usually the 4d-stem has symmetric shear value, we make xy=yx, that's the reason we don't need shear2
             if self.Symmetric:
                 
                 shear_1 = self.shear_limit * nn.Tanh()(out[:, self.count])
@@ -384,17 +387,20 @@ class Affine_Transform(nn.Module):
                 shear_2 = self.shear_limit * nn.Tanh()(out[:, self.count + 1])
 
                 self.count += 2
+                
+        # if there's no shear transformation, shear xy and shear yx should be 0
         else:
             shear_1 = torch.zeros([out.shape[0]]).to(self.device)
             shear_2 = torch.zeros([out.shape[0]]).to(self.device)
 
-        # usually the 4d-stem has symmetric shear value, we make xy=yx, that's the reason we don't need shear2
-
+        
+        # if there's translation transformation, translation x and translation y should be corresponding index of the tensor out
         if self.translation:
             trans_1 = self.trans_limit * nn.Tanh()(out[:, self.count])
             trans_2 = self.trans_limit * nn.Tanh()(out[:, self.count + 1])
             self.count += 2
-            
+        
+        # if there's no translation transformation, translation x and translation y should be 0
         else:
             
             trans_1 = torch.zeros([out.shape[0]]).to(self.device)
@@ -411,7 +417,7 @@ class Affine_Transform(nn.Module):
             # this project doesn't need mask parameter to adjust value intensity in mask region, so we make it 1 here.
             mask_parameter = torch.ones([out.shape[0], 1])
 
-        # reset count to 0 for next minibatch using
+        # reset count to 0 for next minibatch 
         self.count = 0
 
         a_1 = torch.cos(rotate)
@@ -439,17 +445,15 @@ class Affine_Transform(nn.Module):
 
         return scaler_shear, rotation, translation, mask_parameter
 
-
 # narrow the range of the adjust parameter for the mask region, since it is not the noise free dataset,
 # this will increase the background noise's influence to the MSE loss
-#
-class Encoder(nn.Module):
-    """_summary_
 
-    Args:
-        nn (_type_): _description_
+class Encoder(nn.Module):
+    """
+        nn.Module class of Encoder structure, which include affine transformation and base classification
+    Returns:
+        tensor: torch.tensor
     """    
-    #TODO docstring
     
     def __init__(
         self,
@@ -481,7 +485,7 @@ class Encoder(nn.Module):
         interpolate_mode="bicubic",
         affine_mode="bicubic",
     ):
-        """_summary_
+        """
 
         Args:
             original_step_size (list of int): the x and y size of input image
@@ -521,17 +525,20 @@ class Encoder(nn.Module):
         blocks = []
         self.input_size_0 = original_step_size[0]
         self.input_size_1 = original_step_size[1]
+        # set number of blocks depends on length of pool list, each block includes one conv_block and one identity_block
         number_of_blocks = len(pool_list)
         blocks.append(conv_block(t_size=conv_size, n_step=original_step_size))
         blocks.append(identity_block(t_size=conv_size, n_step=original_step_size))
         blocks.append(nn.MaxPool2d(pool_list[0], stride=pool_list[0]))
         for i in range(1, number_of_blocks):
+            # update value of step size for each block
             original_step_size = [
                 original_step_size[0] // pool_list[i - 1],
                 original_step_size[1] // pool_list[i - 1],
             ]
             blocks.append(conv_block(t_size=conv_size, n_step=original_step_size))
             blocks.append(identity_block(t_size=conv_size, n_step=original_step_size))
+            # add MaxPool layer after each block
             blocks.append(nn.MaxPool2d(pool_list[i], stride=pool_list[i]))
 
         self.block_layer = nn.ModuleList(blocks)
@@ -542,7 +549,7 @@ class Encoder(nn.Module):
             original_step_size[0] // pool_list[-1],
             original_step_size[1] // pool_list[-1],
         ]
-
+        # update input_size for linear layer
         input_size = original_step_size[0] * original_step_size[1]
         self.cov2d = nn.Conv2d(
             1, conv_size, 3, stride=1, padding=1, padding_mode="zeros"
@@ -556,6 +563,7 @@ class Encoder(nn.Module):
         self.before = nn.Linear(input_size, reduced_size)
         self.embedding_size = 0
         
+        # determine number of embedding channels depends on affine transformation type
         if scale:
             self.embedding_size += 2
             
@@ -589,17 +597,19 @@ class Encoder(nn.Module):
                 mask_with_inp = []
                 
                 for mask_ in fixed_mask:
+                    # switch mask type into tensor
                     temp_mask = torch.tensor(
                         mask_.reshape(1, 1, self.input_size_0, self.input_size_1),
                         dtype=torch.float,
                     )
-                    
+                    # add the same interpolate as input images to mask
                     temp_mask = F.interpolate(
                         temp_mask,
                         size=(self.up_size, self.up_size),
                         mode=self.interpolate_mode,
                     )
                     
+                    # make the interpolated mask binary ahead to avoid distortion
                     temp_mask[temp_mask < 0.5] = 0
                     temp_mask[temp_mask >= 0.5] = 1
                     temp_mask = torch.tensor(temp_mask.squeeze(), dtype=torch.bool)
@@ -611,7 +621,8 @@ class Encoder(nn.Module):
                 self.mask = fixed_mask
         else:
             self.mask = None
-
+            
+        # if mask_intensity is true, give an extra index of learnable parameter for adjusting pixel intensity
         if mask_intensity:
             self.dense = nn.Linear(reduced_size + num_base, self.embedding_size + 1)
         else:
@@ -629,6 +640,7 @@ class Encoder(nn.Module):
         self.radius = radius
         self.coef = coef
 
+        # initialize affine matrix 
         self.affine_matrix = Affine_Transform(
             device,
             scale,
@@ -661,6 +673,7 @@ class Encoder(nn.Module):
         k_no = kout.clone()
 
         k = self.num_k_sparse
+        # determine input images belongs to which base cluster by top k algorithm, k=1
         with torch.no_grad():
             if k <= kout.shape[1]:
                 for raw in k_no:
@@ -696,6 +709,7 @@ class Encoder(nn.Module):
         out = torch.cat((kout, k_out), dim=1).to(self.device)
         out = self.dense(out)
 
+        # generate affine matrix by tensor out 
         scaler_shear, rotation, translation, mask_parameter = self.affine_matrix(
             out, rotate_value
         )
@@ -715,7 +729,8 @@ class Encoder(nn.Module):
 
         else:
             x_inp = x.view(-1, 1, self.input_size_0, self.input_size_1)
-
+            
+            # image interpolation before affine transformation
             x_inp = F.interpolate(
                 x_inp, size=(self.up_size, self.up_size), mode=self.interpolate_mode
             )
@@ -735,9 +750,9 @@ class Encoder(nn.Module):
             )
             output = F.grid_sample(out_rotate, grid_3)
             
-        # apply inverse affine to each diffraction spot if interpolate is True
+        
         if self.interpolate:
-            
+            # apply inverse affine to each diffraction spot if revise_affine is True
             if self.revise_affine:
             # Test 1.5 is good for 5%-45% background noise, add to 2 for larger noise and rot512x512 4dstem
                 out_revise = revise_size_on_affine_gpu(
@@ -761,18 +776,15 @@ class Encoder(nn.Module):
 
 
 class Decoder(nn.Module):
-    """_summary_
-
-    Args:
-        nn (_type_): _description_
+    """
+        nn.Module class of Decoder (Generator for generating base)
 
     Returns:
-        _type_: _description_
+        tensor: torch.tensor
     """ 
-    #TODO docstring
     
     def __init__(self, original_step_size, up_list, conv_size, device, num_base=2):
-        """_summary_
+        """
 
         Args:
             original_step_size (list of int): the x and y size of input image
@@ -789,6 +801,7 @@ class Decoder(nn.Module):
         self.input_size_0 = original_step_size[0]
         self.input_size_1 = original_step_size[1]
         self.dense = nn.Linear(num_base, original_step_size[0] * original_step_size[1])
+        
         self.cov2d = nn.Conv2d(
             1, conv_size, 3, stride=1, padding=1, padding_mode="zeros"
         )
@@ -796,16 +809,20 @@ class Decoder(nn.Module):
             conv_size, 1, 3, stride=1, padding=1, padding_mode="zeros"
         )
 
+        # set number of blocks depends on length of pool list, each block includes one conv_block and one identity_block
         blocks = []
         number_of_blocks = len(up_list)
         blocks.append(conv_block(t_size=conv_size, n_step=original_step_size))
         blocks.append(identity_block(t_size=conv_size, n_step=original_step_size))
         for i in range(number_of_blocks):
+            
+            # add UpSample layer before each block
             blocks.append(
                 nn.Upsample(
                     scale_factor=up_list[i], mode="bilinear", align_corners=True
                 )
             )
+            # update value of step size for each block
             original_step_size = [
                 original_step_size[0] * up_list[i],
                 original_step_size[1] * up_list[i],
@@ -816,8 +833,8 @@ class Decoder(nn.Module):
         self.block_layer = nn.ModuleList(blocks)
         self.layers = len(blocks)
 
-        self.output_size_0 = original_step_size[0]
-        self.output_size_1 = original_step_size[1]
+        # self.output_size_0 = original_step_size[0]
+        # self.output_size_1 = original_step_size[1]
 
         self.relu_1 = nn.LeakyReLU(0.001)
 
@@ -830,9 +847,8 @@ class Decoder(nn.Module):
         Returns:
             tensor: output tensor
         """
-        # TODO summary
         
-        # reconstruct image into original size
+        # generator to reconstruct image into original size
         out = self.dense(x)
         out = out.view(-1, 1, self.input_size_0, self.input_size_1)
         out = self.cov2d(out)
@@ -845,10 +861,10 @@ class Decoder(nn.Module):
 
 
 class Joint(nn.Module):
-    """_summary_
-
-    Args:
-        nn (_type_): _description_
+    """
+        nn.Module class of VAE, which includes both encoder and decoder
+    Returns:
+        tensor: torch.tensor, affine matrix, interpolated tensor, updated mask list 
     """    
     
     def __init__(
@@ -898,7 +914,7 @@ class Joint(nn.Module):
 
         return self.mask
 
-    # if have pretrained rotation, add to forward function
+    # if have pretrained rotation, add to forward function, and keep optimizing on it
     def forward(self, x, rotate_value=None):
         """
 
@@ -929,18 +945,19 @@ class Joint(nn.Module):
             .repeat(x.shape[0], 1, 1)
             .to(self.device)
         )
-
+        # add identity matrix to affine matrix
         new_theta_1 = torch.cat((scaler_shear, identity), axis=1).to(self.device)
         new_theta_2 = torch.cat((rotation, identity), axis=1).to(self.device)
         new_theta_3 = torch.cat((translation, identity), axis=1).to(self.device)
 
+        # generate inverse affine matrix
         inver_theta_1 = torch.linalg.inv(new_theta_1)[:, 0:2].to(self.device)
         inver_theta_2 = torch.linalg.inv(new_theta_2)[:, 0:2].to(self.device)
         inver_theta_3 = torch.linalg.inv(new_theta_3)[:, 0:2].to(self.device)
 
         predicted_base = self.decoder(k_out)
 
-        # upgrid image is interpolate mode is True
+        # Up grid image when interpolate mode is True
         if self.interpolate:
             
             predicted_base_inp = F.interpolate(
@@ -948,7 +965,7 @@ class Joint(nn.Module):
                 size=(self.up_size, self.up_size),
                 mode=self.interpolate_mode,
             )
-
+            # add inverse affine transform to generated base
             grid_1 = F.affine_grid(
                 inver_theta_1.to(self.device), predicted_base_inp.size()
             ).to(self.device)
@@ -970,6 +987,7 @@ class Joint(nn.Module):
             )
 
         else:
+            # add inverse affine transform to generated base
             grid_1 = F.affine_grid(inver_theta_1.to(self.device), x.size()).to(
                 self.device
             )
@@ -987,10 +1005,11 @@ class Joint(nn.Module):
             predicted_input = F.grid_sample(predicted_rotate, grid_1)
             
         # create new mask list to save updated mask region with inverse affine transform
-
         new_list = []
 
         for mask_ in self.mask:
+            
+            # repeat number of mask to size of minibatch
             batch_mask = (
                 mask_.reshape(1, 1, mask_.shape[-2], mask_.shape[-1])
                 .repeat(x.shape[0], 1, 1, 1)
@@ -1094,7 +1113,7 @@ def make_model_fn(
     interpolate=True,
     revise_affine = False,
 ):
-    """_summary_
+    """function for creating autoencoder and optimizer
 
     Args:
         device (torch.device): set the device initialize model
