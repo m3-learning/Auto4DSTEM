39816b7e7677f1d246ec3ad790573d62ececba75
shuyuqin <shuyuqin@users.noreply.github.com>
2024-09-25 16:11:37 +0000

    deploy: 3c66f5248b066c2aab8765a5e3eced8bb2dee54f

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.post1.dev1+g27b3e08',
+    VERSION: '0.0.post1.dev1+g3c66f52',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
