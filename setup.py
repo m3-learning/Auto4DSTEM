00345c699d7d15e38152a5b22162248782f65f48
Joshua C Agar <jca92@drexel.edu>
2024-09-26 15:45:50 -0400

    Update setup.py #patch

--- setup.py
+++ setup.py
@@ -18,4 +18,4 @@ if __name__ == "__main__":
             "setuptools_scm and wheel with:\n"
             "   pip install -U setuptools setuptools_scm wheel\n\n"
         )
-        raise
+        raise 
