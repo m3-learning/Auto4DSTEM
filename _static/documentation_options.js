54c5e51ea19b4067dd8d242d939d1544825886cb
theodore-s-beers <theodore-s-beers@users.noreply.github.com>
2025-01-23 14:26:14 -0500

    deploy: fb052745d7ab819c48467f9807d8729c269c191f

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.post1.dev1+g32baff7',
+    VERSION: '0.0.post1.dev1+gfb05274',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
