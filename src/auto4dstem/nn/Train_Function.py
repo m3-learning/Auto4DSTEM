1be081350596b1c0926f3279e4118c86a37ea6cd
jagar2 <jca92@drexel.edu>
2025-04-02 10:40:25 -0400

    not working mixin

--- src/auto4dstem/nn/Train_Function.py
+++ src/auto4dstem/nn/Train_Function.py
@@ -23,16 +23,61 @@ from m3util.util.IO import make_folder
 from m3util.viz.text import labelfigs
 
 @dataclass
-class Train:
-    """class of the training process, including load and preprocess the dataset and initialize loss class.
+class IOMixin:
+    """class of the IOMixin process, including load and preprocess the dataset and initialize loss class.
 
     Attributes:
         data_dir (string): directory of the dataset
-        device (torch.device): set the device to run the model. Defaults to torch.device('cpu')
-        seed (int): set the seed to make the training reproducible. Defaults to 42.
-        crop (tuple, optional): the range of index for image cropping. Defaults to ((28,228),(28,228)).
+    """
+    data_dir: str
+
+@dataclass
+class DeviceMixin:
+    """class of the DeviceMixin process, including set the device and seed.
+
+    Attributes:
+        device: torch.device = torch.device("cpu") Set the device to run the model. Defaults to torch.device('cpu')
+        seed: int = 42 Set the seed to make the training reproducible. Defaults to 42.
+    """
+    device: torch.device = torch.device("cpu")
+    seed: int = 42
+    
+@dataclass
+class ImageTransformMixin:
+    """class of the ImageTransformMixin process, including set the crop and transpose.
+
+    Attributes:
+        crop: tuple = ((28,228),(28,228)) Set the crop range for the input image. Defaults to ((28,228),(28,228)).
+        transpose: tuple = (2, 3, 0, 1) Set the transpose for the input image. Defaults to (2, 3, 0, 1). This is used to control the image vs diffraction space. 
+    """
+    crop: tuple = ((28,228),(28,228))
+    transpose: tuple = (2, 3, 0, 1)
+    
+@dataclass
+class NoisyMixin:
+    """class of the NoisyMixin process, including set the noise parameters.
+
+    Attributes:
         background_weight (float, optional): set the intensity of background noise for simulated dataset. Defaults to 0.2.
+    """
+    background_weight: float = 0.2
+    
+@dataclass
+class FineTuningPreTrainMixin:
+    """class of the PreTrainMixin process, including set the pretrained rotation parameters.
+
+    Attributes:
         learned_rotation (numpy array / string, optional): The numpy array/ directory of rotation weights represents pretrained rotation value if exists. Defaults to None.
+        coarse_learned_angle_adjustment (int): The rotation degree added to learned_rotation if exists. Defaults to 0.
+    """
+    learned_rotation: any = None
+    coarse_learned_angle_adjustment: int = 0
+    
+@dataclass
+class Train(IOMixin, DeviceMixin,  ImageTransformMixin, NoisyMixin, FineTuningPreTrainMixin):
+    """class of the training process, including load and preprocess the dataset and initialize loss class.
+
+    Attributes:
         adjust_learned_rotation (int): The rotation degree added to learned_rotation if exists. Defaults to 0.
         background_intensity (bool): determine if the input dataset is simulated data or not. Defaults to True.
         counts_per_probe (float, optional): Counts per probe, can be None or float, defaulting to 1e5.
@@ -112,15 +157,7 @@ class Train:
         visualize_results: Visualizes the results during training.
         save_results: Saves the results during training.
     """
-
-    data_dir: str
-    device: torch.device = torch.device("cpu")
-    seed: int = 42
-    crop: tuple = ((28, 228), (28, 228))
-    transpose: tuple = (2, 3, 0, 1)
-    background_weight: float = 0.2
-    learned_rotation: any = None  # Specify the data type as required
-    adjust_learned_rotation: int = 0
+    
     background_intensity: bool = True
     counts_per_probe: float = 1e5
     intensity_coefficient: float = 1e5 / 4
@@ -222,7 +259,7 @@ class Train:
         # Adjust rotation degree if learned_rotation is provided
         if self.learned_rotation is not None:
             self.learned_rotation = add_disturb(
-                self.learned_rotation, self.adjust_learned_rotation
+                self.learned_rotation, self.coarse_learned_angle_adjustement
             )
 
         # fix seed to reproduce results
