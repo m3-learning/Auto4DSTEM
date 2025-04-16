89c49e64bac95002e26efe4ce161f3f01d7e4ea7
jagar2 <jca92@drexel.edu>
2025-04-16 09:44:18 -0400

    update initialization

--- src/auto4dstem/nn/CC_ST_AE/__init__.py
+++ src/auto4dstem/nn/CC_ST_AE/__init__.py
@@ -1,47 +1,41 @@
-from auto4dstem.nn.CC_ST_AE import FPGA, cc_st_ae, decoder, encoder, ktop
-from auto4dstem.nn.CC_ST_AE.cc_st_ae import build_cc_st_ae
-from auto4dstem.nn.CC_ST_AE.decoder import (
-                                         Decoder,
-)
-from auto4dstem.nn.CC_ST_AE.encoder import (
-                                         Encoder,
-)
-from auto4dstem.nn.CC_ST_AE.FPGA import (
-                                         Decoder_FPGA,
-                                         Encoder_FPGA,
-                                         Joint_FPGA,
-                                         conv_block_fpga,
-                                         identity_block_fpga,
-                                         make_model_fpga,
-)
-from auto4dstem.nn.CC_ST_AE.ktop import (
-                                         ktop_layer,
-)
-from auto4dstem.nn.CC_ST_AE.masks import (
-                                         apply_mask,
-                                         create_square_mask,
-                                         crop_single_diffraction_spot,
-)
-from auto4dstem.nn.CC_ST_AE.network_blocks import (
-                                         AffineTransformationBlock,
-                                         conv_block,
-                                         identity_block,
-)
-from auto4dstem.nn.CC_ST_AE.transforms import (
-                                         intensity_adjustment,
-                                         reverse_affine_transform,
-                                         spatial_transformation,
-)
-from auto4dstem.nn.CC_ST_AE.utils import (
-                                         enforce_transformation_boundary,
-                                         get_coordinate_range,
-)
+from auto4dstem.nn.CC_ST_AE import FPGA
+from auto4dstem.nn.CC_ST_AE import cc_st_ae
+from auto4dstem.nn.CC_ST_AE import decoder
+from auto4dstem.nn.CC_ST_AE import encoder
+from auto4dstem.nn.CC_ST_AE import ktop
+from auto4dstem.nn.CC_ST_AE import masks
+from auto4dstem.nn.CC_ST_AE import network_blocks
+from auto4dstem.nn.CC_ST_AE import transforms
+from auto4dstem.nn.CC_ST_AE import utils
 
-__all__ = ['AffineTransformationBlock', 'cc_st_ae', 'Decoder', 'Decoder_FPGA',
-           'Encoder', 'Encoder_FPGA', 'FPGA', 'Joint_FPGA', 'apply_mask',
-           'conv_block', 'conv_block_fpga', 'create_square_mask',
-           'crop_single_diffraction_spot', 'decoder', 'encoder',
-           'enforce_transformation_boundary', 'get_coordinate_range',
-           'identity_block', 'identity_block_fpga', 'intensity_adjustment',
-           'ktop', 'ktop_layer', 'build_cc_st_ae', 'make_model_fpga',
-           'reverse_affine_transform', 'spatial_transformation']
+from auto4dstem.nn.CC_ST_AE.FPGA import (Decoder_FPGA, Encoder_FPGA,
+                                         Joint_FPGA, conv_block_fpga,
+                                         identity_block_fpga, make_model_fpga,)
+from auto4dstem.nn.CC_ST_AE.cc_st_ae import (CC_ST_AE, build_cc_st_ae,)
+from auto4dstem.nn.CC_ST_AE.decoder import (Decoder,)
+from auto4dstem.nn.CC_ST_AE.encoder import (Encoder,)
+from auto4dstem.nn.CC_ST_AE.ktop import (ktop_layer,)
+from auto4dstem.nn.CC_ST_AE.masks import (apply_mask, create_square_mask,
+                                          crop_single_diffraction_spot,)
+from auto4dstem.nn.CC_ST_AE.network_blocks import (AffineTransformationBlock,
+                                                   conv_block, identity_block,)
+from auto4dstem.nn.CC_ST_AE.transforms import (
+                                               apply_affine_transformation_to_image,
+                                               generate_inverse_affine,
+                                               intensity_adjustment,
+                                               reverse_affine_transform,
+                                               spatial_transformation,)
+from auto4dstem.nn.CC_ST_AE.utils import (enforce_transformation_boundary,
+                                          get_coordinate_range,)
+
+__all__ = ['AffineTransformationBlock', 'CC_ST_AE', 'Decoder', 'Decoder_FPGA',
+           'Encoder', 'Encoder_FPGA', 'FPGA', 'Joint_FPGA',
+           'apply_affine_transformation_to_image', 'apply_mask',
+           'build_cc_st_ae', 'cc_st_ae', 'conv_block', 'conv_block_fpga',
+           'create_square_mask', 'crop_single_diffraction_spot', 'decoder',
+           'encoder', 'enforce_transformation_boundary',
+           'generate_inverse_affine', 'get_coordinate_range', 'identity_block',
+           'identity_block_fpga', 'intensity_adjustment', 'ktop', 'ktop_layer',
+           'make_model_fpga', 'masks', 'network_blocks',
+           'reverse_affine_transform', 'spatial_transformation', 'transforms',
+           'utils']
--- src/auto4dstem/nn/__init__.py
+++ src/auto4dstem/nn/__init__.py
@@ -3,16 +3,21 @@ from auto4dstem.nn import Loss_Function
 from auto4dstem.nn import Train_Function
 from auto4dstem.nn import mixins
 
-from auto4dstem.nn.CC_ST_AE import (CC_ST_AE,
+from auto4dstem.nn.CC_ST_AE import (AffineTransformationBlock, CC_ST_AE,
                                     Decoder, Decoder_FPGA, Encoder,
-                                    Encoder_FPGA, FPGA, Joint_FPGA, conv_block_fpga,
-                                    decoder,
-                                    encoder, identity_block_fpga, ktop, ktop_layer, build_cc_st_ae,
-                                    make_model_fpga,)
-from auto4dstem.nn.CC_ST_AE.masks import apply_mask, create_square_mask, crop_single_diffraction_spot
-from auto4dstem.nn.CC_ST_AE.network_blocks import AffineTransformationBlock, conv_block, identity_block
-from auto4dstem.nn.CC_ST_AE.transforms import intensity_adjustment, reverse_affine_transform, spatial_transformation
-from auto4dstem.nn.CC_ST_AE.utils import enforce_transformation_boundary, get_coordinate_range
+                                    Encoder_FPGA, FPGA, Joint_FPGA,
+                                    apply_affine_transformation_to_image,
+                                    apply_mask, build_cc_st_ae, cc_st_ae,
+                                    conv_block, conv_block_fpga,
+                                    create_square_mask,
+                                    crop_single_diffraction_spot, decoder,
+                                    encoder, enforce_transformation_boundary,
+                                    generate_inverse_affine,
+                                    get_coordinate_range, identity_block,
+                                    identity_block_fpga, intensity_adjustment,
+                                    ktop, ktop_layer, make_model_fpga, masks,
+                                    network_blocks, reverse_affine_transform,
+                                    spatial_transformation, transforms, utils,)
 from auto4dstem.nn.Loss_Function import (AccumulatedLoss,)
 from auto4dstem.nn.Train_Function import (Train,)
 from auto4dstem.nn.mixins import (CenterBeamAlignMixin, DataMixin,
@@ -33,11 +38,13 @@ __all__ = ['AccumulatedLoss', 'AffineTransformationBlock', 'CC_ST_AE',
            'LearnableAffineTransformMixin', 'Loss_Function', 'MaskMixin',
            'ModelHyperParameterMixin', 'ModelMixin', 'NoisyMixin',
            'RegularizationMixin', 'SaveWeightMixin', 'Train', 'Train_Function',
-           'TrainingHyperParameterMixin', 'apply_mask', 'conv_block',
-           'conv_block_fpga', 'create_square_mask',
-           'crop_single_diffraction_spot', 'datamixins', 'decoder', 'encoder',
-           'enforce_transformation_boundary', 'get_coordinate_range',
-           'identity_block', 'identity_block_fpga', 'imagemixins',
-           'intensity_adjustment', 'ktop', 'ktop_layer', 'build_cc_st_ae',
-           'make_model_fpga', 'mixins', 'modelmixins',
-           'reverse_affine_transform', 'spatial_transformation']
+           'TrainingHyperParameterMixin',
+           'apply_affine_transformation_to_image', 'apply_mask',
+           'build_cc_st_ae', 'cc_st_ae', 'conv_block', 'conv_block_fpga',
+           'create_square_mask', 'crop_single_diffraction_spot', 'datamixins',
+           'decoder', 'encoder', 'enforce_transformation_boundary',
+           'generate_inverse_affine', 'get_coordinate_range', 'identity_block',
+           'identity_block_fpga', 'imagemixins', 'intensity_adjustment',
+           'ktop', 'ktop_layer', 'make_model_fpga', 'masks', 'mixins',
+           'modelmixins', 'network_blocks', 'reverse_affine_transform',
+           'spatial_transformation', 'transforms', 'utils']
